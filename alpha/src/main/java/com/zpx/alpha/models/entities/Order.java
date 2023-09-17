486705fbb8ca389826a6f0bf8ca5b549bc362b59
Edgar Jesus <mendesbr92@gmail.com>
2023-09-17 11:47:36 -0300

    add os atributos de Order e OrderItem

--- alpha/src/main/java/com/zpx/alpha/models/entities/Order.java
+++ alpha/src/main/java/com/zpx/alpha/models/entities/Order.java
@@ -1,23 +1,58 @@
 package com.zpx.alpha.models.entities;
 
 
+import jakarta.persistence.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+/**
+ * falta implementar o total
+ * @author ZyphyX
+ */
 
-public class Order {
+@Entity
+@Table
+public class Order implements Serializable {
 
-    Payment payment = new Payment();
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    LocalDateTime momentPag = payment.getMoment();
 
+    LocalDateTime momentPag;
+    OrderStatus orderStatus;
 
+    public Order(){}
+    public Order(Long id,LocalDateTime momentPag,OrderStatus orderStatus){
+        this.id = id;
+        this.momentPag = momentPag;
+        this.orderStatus = orderStatus;
+    }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Order order)) return false;
+        return Objects.equals(getId(), order.getId());
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 
-
-
-
+    public double total(){
+        //TODO
+        throw new UnsupportedOperationException("Metodo não implementado ainda");
+    }
 }
--- alpha/src/main/java/com/zpx/alpha/models/entities/OrderStatus.java
+++ alpha/src/main/java/com/zpx/alpha/models/entities/OrderStatus.java
@@ -1,4 +1,11 @@
 package com.zpx.alpha.models.entities;
 
 public enum OrderStatus {
+
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED
 }
+
